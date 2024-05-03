e1c1760a6473473293a630673f23ec5d1bc87ef2
Scaleupapp <144357944+Scaleupapp@users.noreply.github.com>
2024-05-03 15:17:22 +0530

    Missed committing this for feedback functionality

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -3,9 +3,94 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const UserSettings = require('../models/userSettingsModel');
 const Sentry = require('@sentry/node');
+const Feedback = require('../models/feedbackModel');
+const aws = require('aws-sdk');
 
 require('dotenv').config();
 const jwtSecret = process.env.JWT_SECRET;
+const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const awsRegion = process.env.AWS_REGION;
+
+aws.config.update({
+  accessKeyId: awsAccessKeyId,
+  secretAccessKey: awsSecretAccessKey,
+  region: awsRegion,
+});
+
+const s3 = new aws.S3();
+
+const submitFeedback = async (req, res) => {
+  try {
+    const { name, email, phoneNumber, feedbackType, heading, detail, rating } = req.body;
+    const attachmentFile = req.file;
+
+    // Verify the user's identity using the JWT token
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, jwtSecret);
+
+    // Extract the user ID from the decoded token
+    const userId = decoded.userId;
+
+    const newFeedback = new Feedback({
+      userId,
+      name,
+      email,
+      phoneNumber,
+      feedbackType,
+      heading,
+      detail,
+      rating,
+    });
+
+    // Upload the attachment file to S3 if provided
+    if (attachmentFile) {
+      const uniqueFileName = `${newFeedback._id}_${Date.now()}_${attachmentFile.originalname}`;
+      const params = {
+        Bucket: 'scaleupbucket',
+        Key: `feedback-attachments/${uniqueFileName}`,
+        Body: attachmentFile.buffer,
+        ContentType: attachmentFile.mimetype,
+        ACL: 'public-read',
+      };
+
+      const data = await s3.upload(params).promise();
+      newFeedback.attachmentUrl = data.Location;
+    }
+
+    await newFeedback.save();
+
+    res.status(201).json({ message: 'Feedback submitted successfully' });
+  } catch (error) {
+    Sentry.captureException(error);
+    console.error('Error submitting feedback:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+// Get feedback status controller
+ const getFeedbackStatus = async (req, res) => {
+  try {
+    // Verify the user's identity using the JWT token
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, jwtSecret);
+    const userId = decoded.userId;
+
+    // Find all feedback entries associated with the user's ID
+    const feedback = await Feedback.find({ userId });
+
+    if (!feedback || feedback.length === 0) {
+      return res.status(404).json({ message: 'No feedback found for the logged-in user' });
+    }
+
+    res.json(feedback);
+  } catch (error) {
+    console.error('Error fetching feedback status:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
 
 const changePassword = async (req, res) => {
   try {
@@ -73,7 +158,11 @@ const updateCommentPrivileges = async (req, res) => {
     }
   };
 
+
+
 module.exports = {
   changePassword,
     updateCommentPrivileges,
+    submitFeedback,
+    getFeedbackStatus
 };
